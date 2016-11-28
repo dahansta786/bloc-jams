9e5f89fbfb855e0a78e28cfd1ca4ba3e828ebe99
Hani Anjarwala <dahansta786@Hanis-MacBook-Pro.local>
2016-11-28 17:34:49 -0600

    fixed landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -20,6 +20,6 @@ window.onload = function () {
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
              animatePoints(pointsArray);
-     });
-   }
+     };
+   })
 };
